1043a12c7174bed68f181a458041b90d7cb924b9
chiragkyal <chirag.kyal@gmail.com>
2024-01-16 13:22:11 +0530

    [WIP] unit tests for secret_monitor.go: GetSecrets

--- pkg/route/secret/secret_monitor_test.go
+++ pkg/route/secret/secret_monitor_test.go
@@ -1,11 +1,76 @@
 package secret
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"reflect"
 	"testing"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	fakerest "k8s.io/client-go/rest/fake"
 	"k8s.io/client-go/tools/cache"
 )
+
+type testSecretGetter struct {
+	restClient        *fakerest.RESTClient
+	secrets           []corev1.Secret
+	secretsSerialized io.ReadCloser
+}
+
+func (t *testSecretGetter) WithSecret(secret corev1.Secret) *testSecretGetter {
+	t.secrets = append(t.secrets, secret)
+	return t
+}
+
+func (t *testSecretGetter) WithSerialized() *testSecretGetter {
+	secretList := &v1.SecretList{}
+	secretList.TypeMeta = metav1.TypeMeta{
+		Kind:       "SecretList",
+		APIVersion: "v1",
+	}
+	secretList.Items = t.secrets
+	t.secretsSerialized = serializeObject(secretList)
+	return t
+}
+
+func (t *testSecretGetter) WithRestClient() *testSecretGetter {
+	fakeClient := &fakerest.RESTClient{
+		Client: fakerest.CreateHTTPClient(func(request *http.Request) (*http.Response, error) {
+			resp := &http.Response{
+				StatusCode: http.StatusOK,
+				Header: map[string][]string{
+					"Content-Type": {"application/json"},
+				},
+				Body: t.secretsSerialized,
+			}
+			return resp, nil
+		}),
+		NegotiatedSerializer: scheme.Codecs.WithoutConversion(),
+		GroupVersion:         corev1.SchemeGroupVersion.WithKind("Secret").GroupVersion(),
+		//VersionedAPIPath:     fmt.Sprintf("/api/v1/namespaces/%s/secrets/%s", testNamespace, testSecretName),
+	}
+	t.restClient = fakeClient
+	return t
+}
+
+func serializeObject(o interface{}) io.ReadCloser {
+	output, err := json.MarshalIndent(o, "", "")
+	if err != nil {
+		panic(err)
+	}
+	return ioutil.NopCloser(bytes.NewReader([]byte(output)))
+}
 
 func TestAddSecretEventHandler(t *testing.T) {
 	scenarios := []struct {
@@ -107,4 +172,49 @@ func TestRemoveSecretEventHandler(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestGetSecret(t *testing.T) {
+	var (
+		testNamespace  = "testNamespace"
+		testSecretName = "testSecretName"
+		testRouteName  = "testRouteName"
+	)
+
+	conf := rest.Config{}
+	fakeKubeClient := kubernetes.NewForConfigOrDie(&conf)
+
+	tsg := testSecretGetter{}
+	secret := fakeSecret(testNamespace, testSecretName)
+	tsg.WithSecret(*secret).WithSerialized()
+	tsg.WithRestClient()
+
+	fakeKubeClient.CoreV1().RESTClient().(*rest.RESTClient).Client = tsg.restClient.Client
+	key := NewObjectKey(testNamespace, testRouteName+"_"+testSecretName)
+	sm := secretMonitor{
+		kubeClient: fakeKubeClient,
+		monitors:   map[ObjectKey]*singleItemMonitor{},
+	}
+	h, err := sm.AddSecretEventHandler(key.Namespace, key.Name, cache.ResourceEventHandlerFuncs{})
+	if err != nil || h == nil {
+		t.Error(err)
+	}
+	if !cache.WaitForCacheSync(context.Background().Done(), h.HasSynced) {
+		t.Fatal("cache not synced yet")
+	}
+
+	time.Sleep(2 * time.Second)
+
+	gotSec, gotErr := sm.GetSecret(h)
+	if gotErr != nil {
+		t.Errorf("unexpected error %v", gotErr)
+	}
+
+	if !reflect.DeepEqual(secret, gotSec) {
+		t.Errorf("expected %v got %v", secret, gotSec)
+	}
+
+	// for testing
+	// t.Error("test", gotSec)
+
 }
